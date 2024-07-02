3ffffb1bc77bb5b2a5eeeb2a1e3b34e71646e3e8
Martin Kang <martin.kang@efrei.net>
2024-07-02 17:32:31 +0200

    Adding db file

--- main.py
+++ main.py
@@ -27,9 +27,7 @@ def get_db_connection():
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:8000",
-        "http://localhost:4000",
-        "http://localhost:5173"
+        "http://localhost:*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
